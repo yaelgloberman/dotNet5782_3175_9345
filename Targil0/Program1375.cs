ec4239f9459a3e6b5c254b8fb9938842b85edae9
yael globerman <ygloberm@g.jct.ac.il>
2021-10-10 19:26:29 +0300

    initial program+welcome function

---
+++ Targil0/Program1375.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3175();
+            Welcome9345();
+            Console.ReadKey();
+        }
+        static partial void Welcome9345();
+        private static void Welcome3175()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
