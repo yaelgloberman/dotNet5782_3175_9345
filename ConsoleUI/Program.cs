e1d5c2e53296338b43db173bdaa4bd3f99a92b12
Shifra Wexler <swexler@g.jct.ac.il>
2021-10-25 19:04:55 +0300

    in the process of creating the add case

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,6 +3,7 @@
 namespace ConsoleUI
 {
     public enum MainChoice { add = 1, update, models, list, exit };
+    public enum addChoice {STATION=1, DRONE,CUSTOMER,PARCEL};
     class Program
     {
         static void Main(string[] args)
@@ -10,7 +11,7 @@ namespace ConsoleUI
             //IDAL.DO.BaseStation baseStasion = new IDAL.DO.BaseStation();
             int userA;
             Console.WriteLine("Menue: ");
-            Console.WriteLine("press 1 to add a drone");
+            Console.WriteLine("press 1 to add an object");
             Console.WriteLine("press 2 to update");
             Console.WriteLine("press 3 to display");
             Console.WriteLine("press 4 to see lists ");
@@ -31,25 +32,77 @@ namespace ConsoleUI
                 choice = (MainChoice)num;
                 switch (choice)
                 {
+
                     case MainChoice.add:
                         {
-                            IDAL.DO.Station temp = new IDAL.DO.Station();
-                            Console.WriteLine("enter a station id");
-                            userA = int.Parse(Console.ReadLine());
-                            temp.id = userA;
-                            Console.WriteLine("enter name");
-                            int name1 =int.Parse(Console.ReadLine());
-                            Console.WriteLine("enter longitude");
-                            double longitude1 = double.Parse(Console.ReadLine());
-                            Console.WriteLine("enter latitude");
-                            double latitude1 = double.Parse(Console.ReadLine());
-                            Console.WriteLine("enter charge slots");
-                            int chargeSlots1 = int.Parse(Console.ReadLine());
-                            //temp.set(userA)
-                            DalObject.DalObject.addStation(userA, name1, longitude1, latitude1, chargeSlots1);
+                            Console.WriteLine("what would you like to add?");
+                            Console.WriteLine("enter 1 to add station");
+                            Console.WriteLine("enter 2 to add drone");
+                            Console.WriteLine("enter 3 to add customer");
+                            Console.WriteLine("enter 4 to add parcel");
+                            bool isB;
+                            string str = Console.ReadLine();
+                            //בדיקת תקינות קלט-have to go over that 
+                            isB = int.TryParse(str, out int error1);
+                            int num1;
+                            addChoice choice1;
+                            if (isB)
+                                num1 = int.Parse(str);
+                            else
+                                num1 = -1;
+                            choice1 = (addChoice)num1;
+                            switch (choice1)
+                            {
+                                case addChoice.STATION:
+                                    {
+                                        IDAL.DO.Station temp = new IDAL.DO.Station();
+                                        Console.WriteLine("enter a station id");
+                                        userA = int.Parse(Console.ReadLine());
+                                        temp.id = userA;
+                                        Console.WriteLine("enter name");
+                                        int name1 = int.Parse(Console.ReadLine());
+                                        Console.WriteLine("enter longitude");
+                                        double longitude1 = double.Parse(Console.ReadLine());
+                                        Console.WriteLine("enter latitude");
+                                        double latitude1 = double.Parse(Console.ReadLine());
+                                        Console.WriteLine("enter charge slots");
+                                        int chargeSlots1 = int.Parse(Console.ReadLine());
+                                        //temp.set(userA)
+                                        DalObject.DalObject.addStation(userA, name1, longitude1, latitude1, chargeSlots1);
 
-                            
+                                    }
+                                    break;
+                                case addChoice.DRONE:
+                                    {
+                                        IDAL.DO.Drone temp = new IDAL.DO.Drone();
+                                        Console.WriteLine("enter a drone  id");
+                                        int tmpId= int.Parse(Console.ReadLine());
+                                        temp.Id = tmpId;
+                                        Console.WriteLine("enter Model");
+                                        string tmpModel = Console.ReadLine();
+                                        Console.WriteLine("enter  a number from 1-3 describing its max weight, 3 is the heaviest");//go over the phraising
+                                        IDAL.DO.WeightCatigories weight = (IDAL.DO.WeightCatigories)int.Parse(Console.ReadLine());
+                                        Console.WriteLine("enter the batery status");
+                                        double tmpBateryStatus = double.Parse(Console.ReadLine());
+                                        Console.WriteLine("enter the current drone status");//have to go over to see if i did it right
+                                        IDAL.DO.DroneStatuses tmpDroneStatuses = (IDAL.DO.DroneStatuses)int.Parse(Console.ReadLine());
+                                        //temp.set(userA)
+                                        DalObject.DalObject.addStation(userA, name1, longitude1, latitude1, chargeSlots1);
+                                                public struct Drone
+        {
+           
+
+        }
+                                    break;
+                                case addChoice.CUSTOMER:
+                                    break;
+                                case addChoice.PARCEL:
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
+
                         break;
                     case MainChoice.update:
                         {
@@ -69,7 +122,7 @@ namespace ConsoleUI
                     default:
                         break;
                 }
-            }
+            } while (true);//wrong boolian phrase just trying to minimuze errors - HAVE TO COME BACK TO FIX!! otherwise there will be an infinate loop
         }
     }
 }
