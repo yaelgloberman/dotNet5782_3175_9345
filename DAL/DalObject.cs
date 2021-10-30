b63b8398ee3b8992cc0fd853d628a4c0ff8d322a
Shifra Wexler <swexler@g.jct.ac.il>
2021-10-30 19:58:29 +0300

    some tiud

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -13,8 +13,10 @@ namespace DalObject
         {
             DataSource.Initialize();
         }
+        //*********************** add station functions******************************************//
         public  void addStation(int id1, int name1, double longi, double lati, int charge)//findout if the sattion name is supposed to be string or int
         {
+            // adding all the objects parts 
             Station s = new Station();
             s.id = id1;
             s.name = name1;
@@ -83,33 +85,33 @@ namespace DalObject
                 }
             }
         }
-        public  void SendToCharge(int droneId,int stationId)
+        public  void SendToCharge(int droneId,int stationId)//update function that updates the station and drone when the drone is sent to chatge
         {
-            IDAL.DO.droneCharges dCharge = new droneCharges();          
-            DataSource.drones.ForEach(d => { if (d.Id == droneId) d.Status = DroneStatuses.maintenance; });
-            dCharge.StationId = stationId;
-            stationList().ForEach(s => { if (s.id == stationId) s.chargeSlots--; });
+            IDAL.DO.droneCharges dCharge = new droneCharges();  //creates a new drone object in the drone charges        
+            DataSource.drones.ForEach(d => { if (d.Id == droneId) d.Status = DroneStatuses.maintenance; });// changing the drones status
+            dCharge.StationId = stationId;//maching the drones id
+            stationList().ForEach(s => { if (s.id == stationId) s.chargeSlots--; });//less drone slots in the station
             dCharge.droneId = droneId;
             dCharge.StationId = stationId;
-            chargingDroneList().Add(dCharge);
+            chargingDroneList().Add(dCharge);//adding the drone to the drone chargiong list
         }
-        public  void releasingDrone(droneCharges dC)
+        public  void releasingDrone(droneCharges dC)//update function when we release a drone from its charging slot
         {
-            DataSource.drones.ForEach(d => { if (d.Id == dC.droneId) { d.Status = DroneStatuses.available; d.BateryStatus = 100; } });
-            DataSource.stations.ForEach(s => { if (s.id == dC.StationId) s.chargeSlots++; });
-            DataSource.chargingDrones.Remove(dC);
+            DataSource.drones.ForEach(d => { if (d.Id == dC.droneId) { d.Status = DroneStatuses.available; d.BateryStatus = 100; } });//changing the drone status
+            DataSource.stations.ForEach(s => { if (s.id == dC.StationId) s.chargeSlots++; });//increaseing the drone slots since the drone is finished charging
+            DataSource.chargingDrones.Remove(dC);//removing the drone from the drone charging list
         }
-        public void DeliveryPackageCustomer(int cID,int pId,IDAL.DO.Proirities proirity)
+        public void DeliveryPackageCustomer(int cID,int pId,IDAL.DO.Proirities proirity)//updating the drone when irt was called from the customer
         {
-           parcelList().ForEach(p => { if (p.Id == pId) { p.TargetId = cID;p.Priority = proirity; }; });
+           parcelList().ForEach(p => { if (p.Id == pId) { p.TargetId = cID;p.Priority = proirity; }; });//its proiority is updated - going fast regular or emergency
         }
-        public droneCharges findChargedDrone(int id)
+        public droneCharges findChargedDrone(int id)//finding a drone in the drone charging list
         {
             droneCharges empty = new droneCharges();
             for (int i = 0; i < DataSource.chargingDrones.Count(); i++)
             {
                 if (DataSource.chargingDrones[i].droneId == id)
-                    return DataSource.chargingDrones[i];
+                    return DataSource.chargingDrones[i];// returnong the drone
             }
             return empty;
         }
@@ -165,6 +167,8 @@ namespace DalObject
 
 
     //*********************** printing functions ****************************
+
+        //all the list functions below return the lists of the station drone parcel or customer that was created in the dtata source 
         public List<Station>stationList()
         {
             return DataSource.stations;
@@ -186,7 +190,7 @@ namespace DalObject
             return DataSource.chargingDrones;
         }
  
-        public void MenuPrint(string action)
+        public void MenuPrint(string action)//th menue that helps specify the main action 
         {
             Console.WriteLine($"what would you like to {action}?");
             Console.WriteLine($"enter 1 to {action} station");
